5b103181ed783d63945474a0716ada8acd9636f4
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-12-11 17:10:13 +0600

    Во вторую программу установлена библиотека nodemon. В файл package.json добавлена запись для запуска nodemon через скрипт dev. Написана вторая программа в файле server.js.

--- task2/server.js
+++ task2/server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
-const PORT = 8000;
+const PORT = 8001;
 const Vigenere = require('caesar-salad').Vigenere;
 const KEY = "password";
 
+app.get("/encode/:plainText", (req, res) => {
+    const cypherText = Vigenere.Cipher(KEY).crypt(req.params.plainText);
+    res.send(cypherText);
+});
+
+app.get("/decode/:cypherText", (req, res) => {
+    const plainText = Vigenere.Decipher(KEY).crypt(req.params.cypherText);
+    res.send(plainText);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started at ${PORT} port`);
+});
